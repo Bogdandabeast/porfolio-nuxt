3c7a7c2cc426a1320fdfb98e36e3bdc69e03a38a
Bogdan <bogdan.joboffers@gmail.com>
2025-10-16 09:49:04 +0200

    chore(eslint,prettier,configs) : setup eslint,prettier with settings and recommendations for vscode

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import antfu from '@antfu/eslint-config'
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  antfu({
+    typescript: true,
+    vue: true,
+    nuxt: true,
+    prettier: true,
+    node: false,
+    browser: true,
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-alert': 'error',
+    },
+  }),
+)
