4854dd33c53ad033dd92ba252a68b611a9bed4a0
Bogdan <bogdan.joboffers@gmail.com>
2025-10-16 11:48:46 +0200

    chore(commitlint): commit-lint on commit hook set-up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,8 +14,6 @@ export default withNuxt(
       'no-console': ['error', { allow: ['warn', 'error'] }],
       'no-debugger': 'error',
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'eqeqeq': ['error', 'always'],
-      'curly': ['error', 'all'],
       'no-var': 'error',
       'prefer-const': 'error',
       'no-alert': 'error',
