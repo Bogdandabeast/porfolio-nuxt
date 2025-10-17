97ec59830d126d0bf3ad7acbbc95229640cce8a0
Bogdan <bogdan.joboffers@gmail.com>
2025-10-17 14:19:50 +0200

    feat(tabcomponent): Add Tab category component for projects

---
+++ app/components/project/tabs.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type Category = {
+  name: string;
+}
+
+defineProps<{ categories: Category[] }>();
+
+</script>
+
+<template>
+  <div class="flex justify-center gap-2 md:gap-3 p-3 mb-8 overflow-x-auto">
+    <button
+      v-for="(category, index) in categories"
+      :key="category.name + index"
+      class="flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full px-4 bg-primary/30 text-text-primary text-sm font-medium leading-normal transition-colors"
+    >
+      {{ category.name }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,7 +7,9 @@ import headerContent from '~/assets/data/layout/header.json';
 <template>
   <div>
     <layout-header :header-info="headerContent" />
-    <main><slot /></main>
+    <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+      <slot />
+    </main>
     <layout-footer :footer-info="footerContent" />
   </div>
 </template>
