59004b99b4c5af78521f70d98f14f659fcac3de4
Bogdan <bogdan.joboffers@gmail.com>
2025-10-20 13:57:03 +0200

    feat(transitions): add transitions for switching pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
 
-  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/icon'],
+  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/icon', '@formkit/auto-animate/nuxt'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
